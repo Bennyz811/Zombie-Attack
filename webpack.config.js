e5480af2023057a0111e70c3eee15ab0626d078f
Benny Zhao <bennyz811@gmail.com>
2017-11-09 11:37:01 -0500

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   context: __dirname,
   entry: "./lib/zombie_attack.js",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   module: {
