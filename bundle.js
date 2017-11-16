7f973fae46aae2c7326317cf357d6bd71b8e57d6
Benny Zhao <bennyz811@gmail.com>
2017-11-16 09:05:46 -0500

    edited img link

--- bundle.js
+++ bundle.js
@@ -428,7 +428,7 @@ var Player = function () {
     this.pause = false;
     this.killCount = 0;
     this.heroSprite = new Image();
-    this.heroSprite.src = "./app/assets/images/Hero-Guy-PNG/_Mode-Gun/03-Shot/JK_P_Gun__Attack_007.png";
+    this.heroSprite.src = "app/assets/images/Hero-Guy-PNG/_Mode-Gun/03-Shot/JK_P_Gun__Attack_007.png";
   }
 
   _createClass(Player, [{
